149700dffef52ae76e9a7c3aa0f3772abe747e20
skes2197 <80992374+skes2197@users.noreply.github.com>
2021-03-30 15:41:24 +0100

    Update call.py

--- call.py
+++ call.py
@@ -10,11 +10,11 @@ k = int(input())
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 s.login("skes2197@gmail.com", "payload2 = {'st.r.fieldAcceptCallUIButton': 'Call'}")
-message = "calling " +"+"+str(i) + " pour la " + str(v+1) + "eme fois sur "+str(j)+" pour un laps de"+str(k)+" seconds"
 s = requests.session()
 payload = {'st.r.phone': i}
 payload2 = {'st.r.fieldAcceptCallUIButton': 'Call'}
 for v in range(j):
+    message = "calling " +"+"+str(i) + " pour la " + str(v+1) + "eme fois sur "+str(j)+" pour un laps de"+str(k)+" seconds"
     s.sendmail("skes2197@gmail.com", "skes2197@gmail.com", message)
     s.quit()
     print("--------------")
